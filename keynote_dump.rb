9bca36da4fc236bd379e1148f1013087e0c32c1c
Tom Armitage <tom@infovore.org>
2012-11-26 10:06:16 -0500

    Stupid if statement.

--- keynote_dump.rb
+++ keynote_dump.rb
@@ -32,7 +32,6 @@ def tidy(text)
   text.gsub("<p></p>", "").gsub("\n\n\n", "\n")
 end
 
-
 shortname = ARGV[0]
 template = ARGV[1]
 aspect_ratio = ARGV[2]
@@ -40,42 +39,43 @@ aspect_ratio = ARGV[2]
 unless shortname && template
   puts "Usage: keynote_dump.rb shortname_for_slides template_file [imagemagick_resize_ratio]"
   puts "Optional resize ratio should be of the format (eg) 300x225"
-else
-  if aspect_ratio
-    require 'rmagick'
-    include Magick
-    width = aspect_ratio.split("x").first.to_i
-    height = aspect_ratio.split("x").last.to_i
-    if width && height
-      puts "Resizing all slides in #{shortname}/"
-      Dir.glob("#{shortname}/*.png") do |file|
-        ImageList.new(file).resize(width,height).write(file)
-        print "."
-      end
-      puts 
-    else
-      puts "Invalid resizing format. Slides will not be resized"
-    end
-  end
-
-  puts "Exporting notes from Keynote"
-  keynote = app("Keynote")
-  @notes = []
-
-  keynote.slideshows.get.first.slides.get.each do |slide|
-    output = make_ascii(slide.notes.get)
-    output = create_breaks_around(output)
-    output = tidy(output)
-    @notes << output
-    print "."
-  end
-  puts
-  template = File.open(template) {|f| f.read }
-  
-  output = ERB.new(template)
-  
-  File.open("#{shortname}/#{shortname}.html", "w") do |f|
-    f << output.result
-  end
-  
+  exit
 end
+
+if aspect_ratio
+  require 'rmagick'
+  include Magick
+  width = aspect_ratio.split("x").first.to_i
+  height = aspect_ratio.split("x").last.to_i
+  if width && height
+    puts "Resizing all slides in #{shortname}/"
+    Dir.glob("#{shortname}/*.png") do |file|
+      ImageList.new(file).resize(width,height).write(file)
+      print "."
+    end
+    puts 
+  else
+    puts "Invalid resizing format. Slides will not be resized"
+  end
+end
+
+puts "Exporting notes from Keynote"
+keynote = app("Keynote")
+@notes = []
+
+keynote.slideshows.get.first.slides.get.each do |slide|
+  output = make_ascii(slide.notes.get)
+  output = create_breaks_around(output)
+  output = tidy(output)
+  @notes << output
+  print "."
+end
+puts
+template = File.open(template) {|f| f.read }
+
+output = ERB.new(template)
+
+File.open("#{shortname}/#{shortname}.html", "w") do |f|
+  f << output.result
+end
+
